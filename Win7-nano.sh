ab77f790e2e89ee93175bb10cc936f7c67196ddf
MotSachVN <121332613+MotSachVN@users.noreply.github.com>
2023-01-23 18:50:42 +0700

    Create Win7-nano.sh

---
+++ Win7-nano.sh
@@ -0,0 +1,27 @@
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 5900 &>/dev/null &
+sudo apt-get update && apt-get install qemu -y
+sudo apt install qemu-utils -y
+sudo apt install qemu-system-x86-xen -y
+sudo apt install qemu-system-x86 -y
+wget -0 20GB.qcow2 'https://www.dropbox.com/s/6yzobwz46tzti0l/20GB.qcow2?dl=1'
+wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
+wget -O windows7_super-nano_lite.iso 'https://firebasestorage.googleapis.com/v0/b/baoanbui-ac174.appspot.com/o/Windows%207%2FWindows7_super-nano_lite.iso?alt=media&token=d32c52b1-8481-4c4d-86cc-88483a6d7550'
+
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+sudo qemu-system-x86_64 \
+  -m 8G \
+  -cpu EPYC \
+  -enable-kvm
+  -boot order=d \
+  -drive file=windows7_super-nano_lite.iso,media=cdrom \
+  -drive file=20GB.qcow2 \
+  -drive file=RTL8139F.iso,media=cdrom \
+  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+  -device usb-tablet \
+  -vnc :0 \
+  -cpu core2duo \
+  -smp cores=2 \
+  -device rtl8139,netdev=n0 -netdev user,id=n0 \
+  -accel tcg,thread=multi \
